df2a3daf41768815bef515435b10b71733ff2ae1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-06-25 16:26:33 +0200

    [FIX] Grammar
    There was a superfluous be.

--- spec/NetworkSpec.js
+++ spec/NetworkSpec.js
@@ -1,5 +1,5 @@
 describe("A Network", function(){
-    it("should be start out with zero Points", function(){
+    it("should start out with zero Points", function(){
 	var network = new Network();
 
 	expect(network.numberOfPoints()).toBe(0);
