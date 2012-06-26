2e19137b42386c05f71c442c8ec4e43a87ea01a6
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-06-26 17:28:18 +0200

    [REFACTOR] Abstracted point construction
    The functions used in tests to construct invalid points are abstracted
    in a higher-level function.

--- spec/PointSpec.js
+++ spec/PointSpec.js
@@ -1,17 +1,13 @@
 describe("A Point", function(){
     it("should be created with both an x and y coordinate", function(){
-	var noArguments = function(){
-	    new MonaLisa.Point();
-	};
-	var xArguments = function(){
-	    new MonaLisa.Point({ x : 0 });
-	};
-	var yArguments = function(){
-	    new MonaLisa.Point({ y : 0 });
-	};
+	var aPointConstructedWith = function(arguments) {
+	    return function(){
+		new MonaLisa.Points(arguments);
+	    }
+	}
 
-	expect(noArguments).toThrow();
-	expect(xArguments).toThrow();
-	expect(yArguments).toThrow();
+	expect(aPointConstructedWith({})).toThrow();
+	expect(aPointConstructedWith({ x : 0 })).toThrow();
+	expect(aPointConstructedWith({ y : 0 })).toThrow();
     });
 });
