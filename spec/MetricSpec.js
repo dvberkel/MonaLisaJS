81c35360fe3fde82d44160d2cf6c1b5699170bf2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-06-27 10:33:46 +0200

    Manhatten distance
    The manhatten distance is added as an example of a different metric.

--- spec/MetricSpec.js
+++ spec/MetricSpec.js
@@ -12,4 +12,18 @@ describe("A Metric", function(){
 	    expect(d(e1, e2)).toBeCloseTo(Math.sqrt(2));
 	});
     });
+
+    describe("(Manhatten)", function(){
+	it("should determine 'distance' between Points", function(){
+	    var origin = new MonaLisa.Point({ x : 0, y : 0 });
+	    var e1 = new MonaLisa.Point({ x : 1, y : 0 });
+	    var e2 = new MonaLisa.Point({ x :  0, y : 1 });
+
+	    var d = MonaLisa.Metric.manhatten;
+
+	    expect(d(origin, origin)).toBeCloseTo(0.0);
+	    expect(d(origin, e1)).toBeCloseTo(1.0);
+	    expect(d(e1, e2)).toBeCloseTo(2);
+	});
+    });
 });
--- src/Metric.js
+++ src/Metric.js
@@ -8,6 +8,10 @@
     var squareOf = function(z) {
 	return z * z;
     };
+
+    var absolute = function(z) {
+	return Math.abs(z);
+    };
     
     var differenceOf = function(a, b) {
 	return a - b;
@@ -15,5 +19,9 @@
 
     MonaLisa.Metric.euclidean = function(a, b){
 	return squareRootOf(squareOf(differenceOf(a.x(), b.x())) + squareOf(differenceOf(a.y(), b.y())));
+    };
+
+    MonaLisa.Metric.manhatten = function(a, b){
+	return (absolute(differenceOf(a.x(), b.x())) + absolute(differenceOf(a.y(), b.y())));
     };
 })(MonaLisa);
