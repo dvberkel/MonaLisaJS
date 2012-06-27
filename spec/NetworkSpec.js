db3ea17175dde98cde1de18884efe85c1ff753fb
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-06-27 10:40:23 +0200

    [REFACTOR] beforeEach used
    The beforeEach construct is used in the NetworkSpec.

--- spec/NetworkSpec.js
+++ spec/NetworkSpec.js
@@ -1,13 +1,15 @@
 describe("A Network", function(){
-    it("should start out with zero Points", function(){
-	var network = new MonaLisa.Network();
+    var network;
 
+    beforeEach(function(){
+	network = new MonaLisa.Network();
+    });
+
+    it("should start out with zero Points", function(){
 	expect(network.numberOfPoints()).toBe(0);
     });
 
     it("should be build up by adding Points", function(){
-	var network = new MonaLisa.Network();
-
 	network.addPoint(new MonaLisa.Point({ x : 0, y : 0 }));
 	network.addPoint(new MonaLisa.Point({ x : 1, y : 0 }));
 
