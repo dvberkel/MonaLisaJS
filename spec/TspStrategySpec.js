ff5ab211b1eb22d35451ec951934e43a2c852198
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-06-27 17:28:43 +0200

    [REFACTOR] Cycle Network
    The cycle network is introduced. It constructs a (circle) network with
    a known optimal path lenght.
    
    Proof-idea: When a path crosses itself it is not optimal. The only non-crossing
    path is along the perimeter

--- spec/TspStrategySpec.js
+++ spec/TspStrategySpec.js
@@ -1,14 +1,19 @@
+var cycleNetworkOfSize = function(n) {
+    var alpha = 2 * Math.PI / n;
+    var r = 1 / (2 * Math.sin(alpha / 2));
+    var network = new MonaLisa.Network();
+    for (var index = 0; index < n; index++) {
+	network.addPoint(new MonaLisa.Point({ x : r * Math.cos(alpha * index), y : r * Math.sin(alpha * index) }));
+    }
+    return network;
+}
+
 describe("A TspStrategy", function(){
     describe("(brute force)", function(){
 	it("should return the shortest possible route between all points", function(){
 	    var strategy = MonaLisa.Strategy.BruteForce.withMetric(MonaLisa.Metric.euclidean);
-	    var network = new MonaLisa.Network();
-	    network.addPoint(new MonaLisa.Point({ x : 0, y : 0 }));
-	    network.addPoint(new MonaLisa.Point({ x : 1, y : 0 }));
-	    network.addPoint(new MonaLisa.Point({ x : 1, y : 1 }));
-	    network.addPoint(new MonaLisa.Point({ x : 0, y : 1 }));
 
-	    var path = strategy.solveFor(network);
+	    var path = strategy.solveFor(cycleNetworkOfSize(4));
 
 	    expect(path).toHaveALengthCloseTo(4.0);;
 	});
@@ -17,13 +22,8 @@ describe("A TspStrategy", function(){
     describe("(greedy)", function(){
 	it("should return a short route between all points", function(){
 	    var strategy = MonaLisa.Strategy.Greedy.withMetric(MonaLisa.Metric.euclidean);
-	    var network = new MonaLisa.Network();
-	    network.addPoint(new MonaLisa.Point({ x : 0, y : 0 }));
-	    network.addPoint(new MonaLisa.Point({ x : 1, y : 0 }));
-	    network.addPoint(new MonaLisa.Point({ x : 1, y : 1 }));
-	    network.addPoint(new MonaLisa.Point({ x : 0, y : 1 }));
 
-	    var path = strategy.solveFor(network);
+	    var path = strategy.solveFor(cycleNetworkOfSize(4));
 
 	    expect(path).toHaveALengthCloseTo(4.0);;
 	});
@@ -32,13 +32,8 @@ describe("A TspStrategy", function(){
     describe("(random)", function(){
 	it("should return a route between all points", function(){
 	    var strategy = MonaLisa.Strategy.Random.withMetric(MonaLisa.Metric.euclidean);
-	    var network = new MonaLisa.Network();
-	    network.addPoint(new MonaLisa.Point({ x : 0, y : 0 }));
-	    network.addPoint(new MonaLisa.Point({ x : 1, y : 0 }));
-	    network.addPoint(new MonaLisa.Point({ x : 1, y : 1 }));
-	    network.addPoint(new MonaLisa.Point({ x : 0, y : 1 }));
 
-	    var path = strategy.solveFor(network);
+	    var path = strategy.solveFor(cycleNetworkOfSize(4));
 
 	    expect(path).toBeDefined();;
 	});
