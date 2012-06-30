bf0c577751b8328d2cd823c5c7f0d1e520550a5c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-06-30 11:07:21 +0200

    [REFACTOR] BeforeEach used
    An beforeEach block is used to abstract the loading of the fixtures
    and the construction of the paper.

--- spec/NetworkViewSpec.js
+++ spec/NetworkViewSpec.js
@@ -1,8 +1,15 @@
 describe("A NetworkView", function(){
-    it("should add points to the svg element", function(){
+    beforeEach(function(){
 	loadFixtures('viewport.html');
+	this.paper = Raphael("viewport", 460, 320);
+    });
 
-	var paper = Raphael("viewport", 460, 320);
+    it("should work on an existing paper", function(){
+	expect($("svg")).toExist();
+    });
+    
+    it("should add points to the paper", function(){
+	var paper = this.paper;
 	
 	var network = new MonaLisa.Network();
 	network.addPoint(new MonaLisa.Point({ x : 1, y : 0 }));
