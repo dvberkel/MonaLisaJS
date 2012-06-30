e88f8e4d4d39507fedef00562931645baabf9206
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-06-30 11:02:53 +0200

    Jasmine jquery spec
    The Network view is spec with the jasmine jqeury library.

---
+++ spec/NetworkViewSpec.js
@@ -0,0 +1,20 @@
+describe("A NetworkView", function(){
+    it("should add points to the svg element", function(){
+	loadFixtures('viewport.html');
+
+	var paper = Raphael("viewport", 460, 320);
+	
+	var network = new MonaLisa.Network();
+	network.addPoint(new MonaLisa.Point({ x : 1, y : 0 }));
+	network.addPoint(new MonaLisa.Point({ x : 0, y : 1 }));
+
+	new MonaLisa.NetworkView({
+	    model : network,
+	    paper : paper,
+	    transformX : function(x){return 50 * x + 230 },
+	    transformY : function(y){return 50 * y + 160 }
+	})
+
+	expect($("circle")).toExist();
+    });
+});
