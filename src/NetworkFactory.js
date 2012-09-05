a69eead077753fe451552eb27d313600c797d002
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-09-05 10:14:07 +0200

    Standard Networks expanded
    The standard network are expanded with a grid network.
    I demonstrates the weakness of hte greedy algorithm. I.e
    local optimalisation versus global optimalisation.

--- src/NetworkFactory.js
+++ src/NetworkFactory.js
@@ -8,6 +8,15 @@
 		network.addPoint(new MonaLisa.Point({ x : r * Math.cos(alpha * index), y : r * Math.sin(alpha * index) }));
 	    }
 	    return network;
+	},
+	gridNetworkOfSize : function(width, height){
+	    var network = new MonaLisa.Network();
+	    for (var x = 0; x < width; x++) {
+		for (var y = 0; y < height; y++) {
+		    network.addPoint(new MonaLisa.Point({ x : x, y : y}));
+		}
+	    }
+	    return network;
 	}
     };
 
--- src/showcase.js
+++ src/showcase.js
@@ -3,22 +3,44 @@
 	var paper = Raphael("viewport", 460, 320);
 	paper.rect(0, 0, 460, 320).attr("fill", "ivory");
 
-	var network = MonaLisa.NetworkFactory.cycleNetworkOfSize(10);
+	var cycle = MonaLisa.NetworkFactory.cycleNetworkOfSize(10);
+	var cycleTransformX = function(x){ return 40 * x + 115; };
+	var cycleTransformY = function(y){ return 40 * y + 80; };
 
 	new MonaLisa.NetworkView({
-	    model : network,
+	    model : cycle,
 	    paper : paper,
-	    transformX : function(x){ return 50 * x + 230 },
-	    transformY : function(y){ return 50 * y + 160 }
+	    transformX : cycleTransformX,
+	    transformY : cycleTransformY
 	});
 
-	var path = MonaLisa.Strategy.Greedy.withMetric(MonaLisa.Metric.euclidean).solveFor(network);
+	var cyclePath = MonaLisa.Strategy.Greedy.withMetric(MonaLisa.Metric.euclidean).solveFor(cycle);
 
 	new MonaLisa.PathView({
-	    model : path,
+	    model : cyclePath,
 	    paper : paper,
-	    transformX : function(x){ return 50 * x + 230 },
-	    transformY : function(y){ return 50 * y + 160 }
+	    transformX : cycleTransformX,
+	    transformY : cycleTransformY
+	});
+
+ 	var grid = MonaLisa.NetworkFactory.gridNetworkOfSize(8, 5);
+	var gridTransformX = function(x){ return 30 * x + 230; };
+	var gridTransformY = function(y){ return 30 * y + 160; };
+
+	new MonaLisa.NetworkView({
+	    model : grid,
+	    paper : paper,
+	    transformX : gridTransformX,
+	    transformY : gridTransformY
+	});
+
+	var gridPath = MonaLisa.Strategy.Greedy.withMetric(MonaLisa.Metric.euclidean).solveFor(grid);
+
+	new MonaLisa.PathView({
+	    model : gridPath,
+	    paper : paper,
+	    transformX : gridTransformX,
+	    transformY : gridTransformY
 	});
     });
 })(jQuery, Raphael,  MonaLisa);
