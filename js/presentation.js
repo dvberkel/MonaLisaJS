5eececbb1c2f5024f1377a2aab8ce6e21e461be9
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-10-04 15:23:46 +0200

    final monalisa items

--- js/presentation.js
+++ js/presentation.js
@@ -18,6 +18,21 @@
 	  
 	$("#non-crossing").non_crossing();
 
+	var paper = Raphael("viewport", 420, 640);
+	
+	var monalisa = monalisa_network;
+	var monalisaTransformX = function(x){ return 2*x/3 - 850; };
+	var monalisaTransformY = function(y){ return 2*y/3 + 75; };
+
+	var monalisaPath = MonaLisa.Strategy.Greedy.withMetric(MonaLisa.Metric.euclidean).solveFor(monalisa);
+
+	new MonaLisa.PathView({
+	    model : monalisaPath,
+	    paper : paper,
+	    transformX : monalisaTransformX,
+	    transformY : monalisaTransformY
+	});
+
 	$("#toc").toc({
 	    'selectors' : 'h2',
 	    'container' : 'body',
